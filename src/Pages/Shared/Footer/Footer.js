e8b1e3bc92f24dd05a3eb8a01374cf0b3b2f6483
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-03 14:40:43 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.svg'
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className='bg-black'>
+            <footer className="footer p-10 container mx-auto text-white">
+                <div>
+                    <Link to='/'>
+                        <img src={logo} alt="" />
+                    </Link>
+                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link className="link link-hover">Branding</Link>
+                    <Link className="link link-hover">Design</Link>
+                    <Link className="link link-hover">Marketing</Link>
+                    <Link className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                    <Link className="link link-hover">Jobs</Link>
+                    <Link className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link className="link link-hover">Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Cookie policy</Link>
+                </div>
+            </footer>
         </div>
     );
 };
