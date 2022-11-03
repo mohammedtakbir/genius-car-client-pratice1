85d3ffa59f3799558d5b7f1888c4e3934c2a53f8
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-03 16:22:03 +0600

    add service section

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ServiceCard = ({ service }) => {
+    const { title, img, price, _id } = service;
+
+    return (
+        <>
+            <div className="card card-compact bg-base-100 shadow-xl border p-4 min-h-full">
+                <figure><img src={img} alt="Shoes" className='rounded-lg' /></figure>
+                <div className="mt-4">
+                    <h2 className="font-medium text-2xl mb-3">{title}</h2>
+                    <p className='text-xl font-medium mb-3 text-green-400'>Price: ${price}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-success text-white">checkout</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ServiceCard;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
+    return (
+        <div className='container mx-auto'>
+            <div className='text-center mt-6'>
+                <h1 className="text-xl font-bold text-green-400">Services</h1>
+                <h3 className='text-5xl font-bold mb-5 mt-3'>Our Service Area</h3>
+                <p className="text-gray-500 w-[35%] mx-auto mb-12">
+                    the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+                </p>
+            </div>
+            <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
